3855a49d589fc915817a5babfde7816d7f745733
vandit vasa <vasavandit@gmail.com>
2024-09-14 23:19:57 +0530

    feat: added endpoint for transactions

--- controllers/client/profile.controller.js
+++ controllers/client/profile.controller.js
@@ -1,4 +1,5 @@
 import { CardDb } from '../../models/associations.js';
+import Transactions from '../../models/transactions.model.js';
 import ApiError from '../../utils/ApiError.js';
 
 export const updateProfile = async (req, res) => {
@@ -51,4 +52,25 @@ export const updateProfile = async (req, res) => {
   return res
     .status(200)
     .send({ message: 'Profile Updated', data: updatedProfileData });
+};
+
+export const transactions = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = parseInt(req.query.page_size) || 10;
+  const offset = (page - 1) * pageSize;
+
+  const transactions = await Transactions.findAll({
+    where: {
+      cardno: req.user.cardno
+    },
+    attributes: {
+      exclude: ['id', 'cardno', 'updatedAt', 'updatedBy']
+    },
+    order: [['createdAt', 'DESC']],
+    offset,
+    limit: pageSize
+  });
+  return res
+    .status(200)
+    .send({ message: 'fetched transactions', data: transactions });
 };
