35b186e1c82445160b94a91f9ff48d9bf75ccd8e
vandit vasa <vasavandit@gmail.com>
2024-10-06 19:10:15 +0530

    Fix: Display food bookings in chronological order

--- controllers/client/foodBooking.controller.js
+++ controllers/client/foodBooking.controller.js
@@ -177,7 +177,7 @@ export const FetchFoodBookings = async (req, res) => {
         cardno: req.query.cardno,
         ...dateFilter // Apply date filter if provided
       },
-      order: [['date', 'DESC']],
+      order: [['date', 'ASC']],
       offset,
       limit: pageSize
     }),
@@ -193,7 +193,7 @@ export const FetchFoodBookings = async (req, res) => {
           attributes: ['name']
         }
       ],
-      order: [['date', 'DESC']],
+      order: [['date', 'ASC']],
       offset,
       limit: pageSize
     })
