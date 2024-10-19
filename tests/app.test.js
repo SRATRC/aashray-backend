116fc298904cb7b6eb3477647366906430ec7e9d
Abhishek Jain <abhishek@liveramp.com>
2024-10-19 01:26:38 -0700

    add test for invalid url

--- tests/app.test.js
+++ tests/app.test.js
@@ -16,13 +16,25 @@ beforeAll(async () => {
 });
 
 describe('GET /', () => {
-  it('should return 200 and "API is up and running... 🚀"', async () => {
-    const response = await request(app).get('/');
-    expect(response.status).toBe(200);
-    // expect(response.text).toBe('API is up and running... 🚀');
+  it('should return 200', async () => {
+    const response = await request(app)
+      .get('/')
+      .expect(200);
+    expect(response.body.data).toBe('API is up and running... 🚀');
+    expect(response.body.status).toBe(200);
   });
 });
 
+describe('GET /some/random/url', () => {
+  it('should return 404', async () => {
+    const response = await request(app)
+      .get('/some/random/url')
+      .expect(404);
+
+      expect(response.body.message).toBe('Page Not Found');
+      expect(response.body.statusCode).toBe(404);
+  });
+});
 
 // Close the database connection after all tests
 afterAll(async () => {
