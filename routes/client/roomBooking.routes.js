12f46814659c3b7379890691ed9d84791397e021
Abhishek Jain <abhishek@abhisheks-mbp.lan>
2025-01-29 22:09:25 -0800

    add flat booking routes back

--- routes/client/roomBooking.routes.js
+++ routes/client/roomBooking.routes.js
@@ -4,6 +4,7 @@ import {
   AvailabilityCalender,
   ViewAllBookings,
   CancelBooking,
+  FlatBookingForMumukshuAndGuest
 } from '../../controllers/client/roomBooking.controller.js';
 import CatchAsync from '../../utils/CatchAsync.js';
 import { validateCard, CheckDatesBlocked } from '../../middleware/validate.js';
@@ -12,6 +13,7 @@ router.use(validateCard);
 
 router.get('/availablity', CatchAsync(AvailabilityCalender));
 router.post('/cancel', CatchAsync(CancelBooking));
+router.post('/flat', CatchAsync(FlatBookingForMumukshuAndGuest));
 router.get('/bookings', CatchAsync(ViewAllBookings));
 
 export default router;
