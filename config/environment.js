a630eb78d2c795d9fc6403b7cb8f9f3f4f7e0005
vandit vasa <vasavandit@gmail.com>
2025-03-21 15:30:55 +0530

    feat: added logging

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ import logger from './logger.js';
 const env = process.env.NODE_ENV || 'dev';
 const envFilePath = resolve('./', `.env.${env}`);
 
+config({ path: envFilePath });
+
 logger.info(`Environment loaded: ${env}`);
 logger.info(`Environment file: ${envFilePath}`);
-
-config({ path: envFilePath });
