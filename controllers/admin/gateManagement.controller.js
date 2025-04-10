703d4374fcac2ea9c1d355235cedcc626900a154
vandit vasa <vasavandit@gmail.com>
2025-04-10 17:14:40 +0530

    merging gate management

--- controllers/admin/gateManagement.controller.js
+++ controllers/admin/gateManagement.controller.js
@@ -1,11 +1,18 @@
-import { CardDb } from '../../models/associations.js';
+import { GateRecord, CardDb, FlatBooking } from '../../models/associations.js';
 import {
   STATUS_MUMUKSHU,
   STATUS_ONPREM,
   STATUS_RESIDENT,
-  STATUS_SEVA_KUTIR
+  STATUS_SEVA_KUTIR,
+  STATUS_OFFPREM,
+  ROOM_STATUS_CHECKEDIN,
+  ROOM_STATUS_CHECKEDOUT,
+  ROOM_STATUS_PENDING_CHECKIN
 } from '../../config/constants.js';
 import Sequelize from 'sequelize';
+import moment from 'moment';
+import database from '../../config/database.js';
+import ApiError from '../../utils/ApiError.js';
 
 export const fetchTotal = async (req, res) => {
   const result = await CardDb.findAll({
@@ -69,4 +76,87 @@ export const fetchSevaKutir = async (req, res) => {
   });
 
   return res.status(200).send({ message: 'Success', data: total_seva });
+};
+
+export const gateEntry = async (req, res) => {
+  const t = await database.transaction();
+  req.transaction = t;
+
+  const user = await CardDb.findOne({
+    where: { cardno: req.params.cardno }
+  });
+
+  user.update(
+    { status: STATUS_ONPREM, updatedBy: req.user.username },
+    { transaction: t }
+  );
+
+  await GateRecord.create(
+    {
+      cardno: req.params.cardno,
+      status: STATUS_ONPREM,
+      updatedBy: req.user.username
+    },
+    { transaction: t }
+  );
+
+  const today = moment().format('YYYY-MM-DD');
+
+  const booking = await FlatBooking.findOne({
+    where: {
+      cardno: req.params.cardno,
+      status: ROOM_STATUS_PENDING_CHECKIN,
+      checkin: { [Sequelize.Op.lte]: today },
+      checkout: { [Sequelize.Op.gte]: today }
+    }
+  });
+
+  if (booking) {
+    booking.status = ROOM_STATUS_CHECKEDIN;
+    await booking.save({ transaction: t });
+  }
+
+  await t.commit();
+  return res.status(200).send({ message: 'Success' });
+};
+
+export const gateExit = async (req, res) => {
+  const t = await database.transaction();
+  req.transaction = t;
+
+  const user = await CardDb.findOne({
+    where: { cardno: req.params.cardno }
+  });
+
+  user.update(
+    { status: STATUS_OFFPREM, updatedBy: req.user.username },
+    { transaction: t }
+  );
+
+  await GateRecord.create(
+    {
+      cardno: req.params.cardno,
+      status: STATUS_OFFPREM,
+      updatedBy: req.user.username
+    },
+    { transaction: t }
+  );
+
+  const today = moment().format('YYYY-MM-DD');
+
+  const booking = await FlatBooking.findOne({
+    where: {
+      cardno: req.params.cardno,
+      status: ROOM_STATUS_CHECKEDIN,
+      checkout: { [Sequelize.Op.lte]: today }
+    }
+  });
+
+  if (booking) {
+    booking.status = ROOM_STATUS_CHECKEDOUT;
+    await booking.save({ transaction: t });
+  }
+
+  await t.commit();
+  return res.status(200).send({ message: 'Success' });
 };
