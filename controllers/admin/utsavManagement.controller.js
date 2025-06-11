b158c66374cdbf02b811b8e739b24f6ca064ae7c
Abhishek Jain <abhishek@abhisheks-mbp.lan>
2025-06-11 09:16:01 -0700

    don't allow transactions with CANCELLED status to be cancelled as well

--- controllers/admin/utsavManagement.controller.js
+++ controllers/admin/utsavManagement.controller.js
@@ -19,7 +19,8 @@ import {
   STATUS_CASH_COMPLETED,
   STATUS_CASH_PENDING,
   TYPE_UTSAV,
-  STATUS_CREDITED
+  STATUS_CREDITED,
+  STATUS_CANCELLED
 } from '../../config/constants.js';
 import Transactions from '../../models/transactions.model.js';
 import database from '../../config/database.js';
@@ -482,7 +483,7 @@ export const utsavStatusUpdate = async (req, res) => {
 
       if (
         transaction &&
-        ![STATUS_CREDITED, STATUS_ADMIN_CANCELLED].includes(transaction.status)
+        ![STATUS_CREDITED, STATUS_CANCELLED, STATUS_ADMIN_CANCELLED].includes(transaction.status)
       ) {
         await adminCancelTransaction(req.user, transaction, t);
         console.log('>> Cancelling transaction...');
