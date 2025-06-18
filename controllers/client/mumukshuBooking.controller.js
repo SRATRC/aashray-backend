805788833a775ae1ea5704a339fdcfd5232b05d2
vandit vasa <vasavandit@gmail.com>
2025-06-18 11:58:44 +0530

    not generating orderid for international users

--- controllers/client/mumukshuBooking.controller.js
+++ controllers/client/mumukshuBooking.controller.js
@@ -96,9 +96,12 @@ export const mumukshuBooking = async (req, res) => {
     }
   }
 
-  const order = await generateOrderId(amount);
-  const bookingIds = retrieveBookingIds(userBookingIdMap);
-  await updateRazorpayTransactions(bookingIds, [], order.id, t);
+  var order = null;
+  if (req.user.country == 'India' && amount > 0) {
+    order = await generateOrderId(amount);
+    const bookingIds = retrieveBookingIds(userBookingIdMap);
+    await updateRazorpayTransactions(bookingIds, [], order.id, t);
+  }
   await t.commit();
 
   //Sending email to logged in user for self or other mumkshus
@@ -128,9 +131,11 @@ export const mumukshuBooking = async (req, res) => {
       ? MSG_BOOKING_WAITING
       : MSG_BOOKING_SUCCESSFUL;
 
-  return res
-    .status(200)
-    .send({ message: message, order, waitingBookingCountMap });
+  return res.status(200).send({
+    message: message,
+    order: order ? order : { amount: 0 },
+    waitingBookingCountMap
+  });
 };
 
 export const validateBooking = async (req, res) => {
