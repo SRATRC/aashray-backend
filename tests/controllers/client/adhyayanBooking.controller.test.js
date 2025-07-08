6fffdaf419eed0e71d6b4aac361e1acead122b32
Abhishek Jain <abhishek@Abhisheks-MacBook-Pro.local>
2025-07-08 22:52:26 +0530

    todo tests

--- tests/controllers/client/adhyayanBooking.controller.test.js
+++ tests/controllers/client/adhyayanBooking.controller.test.js
@@ -4,7 +4,7 @@ import { app, sequelize } from '../../../app';
 import { CardDb } from '../../../models/associations';
 import { STATUS_CONFIRMED, STATUS_CANCELLED, TYPE_ADHYAYAN } from '../../../config/constants';
 
-describe('Cancel Shibir', () => {
+describe('Adhyayan Booking Controller', () => {
   beforeAll(() => {
     jest.spyOn(CardDb, 'findOne').mockResolvedValue({
       id: 1,
@@ -12,37 +12,17 @@ describe('Cancel Shibir', () => {
     });
   });
   
-
+  
   describe('Cancel Shibir', () => {
-    // it('should cancel a shibir booking', async () => {
-    //   // Create a shibir and a booking in the database
-    //   const shibir = await ShibirDb.create({
-    //     name: 'Test Shibir',
-    //     description: 'This is a test shibir',
-    //     date: new Date(),
-    //   });
-    //   const booking = await ShibirBookingDb.create({
-    //     shibirId: shibir.id,
-    //     status: STATUS_CONFIRMED,
-    //     type: TYPE_ADHYAYAN,
-    //   });
 
-    //   const res = await request(app)
-    //     .delete(`/api/v1/adhyayan/cancel`)
-    //     .send({ bookingid: booking.id });
+    test.todo('should cancel self booking successfully');
+    test.todo('should cancel guest or mumukshu booking successfully');
 
-    //   expect(res.status).toBe(200);
-    //   expect(res.body).toEqual({
-    //     id: booking.id,
-    //     shibirId: shibir.id,
-    //     status: STATUS_CANCELLED,
-    //     type: TYPE_ADHYAYAN,
-    //   });
+    test.todo('should return an error when cancelling a cancelled booking');
+    test.todo('should open a seat when cancelling a confirmed booking');
+    test.todo('should open a seat when cancelling a payment pending booking');
+    test.todo('should send email notification when cancelling a booking');
 
-    //   // Verify that the booking status has been updated in the database
-    //   const updatedBooking = await ShibirBookingDb.findByPk(booking.id);
-    //   expect(updatedBooking.status).toBe(STATUS_CANCELLED);
-    // });
 
     it('should return an error when cancelling a non-existent booking', async () => {
       const res = await request(app)
@@ -52,31 +32,7 @@ describe('Cancel Shibir', () => {
       expect(res.status).toBe(404);
       expect(res.body.message).toEqual('Booking not found');
     });
-
-    // it('should return an error when cancelling a booking that is already cancelled', async () => {
-    //   // Create a shibir and a booking in the database
-    //   const shibir = await ShibirDb.create({
-    //     name: 'Test Shibir',
-    //     description: 'This is a test shibir',
-    //     date: new Date(),
-    //   });
-    //   const booking = await ShibirBookingDb.create({
-    //     shibirId: shibir.id,
-    //     status: STATUS_CANCELLED,
-    //     type: TYPE_ADHYAYAN,
-    //   });
-
-    //   const res = await request(app)
-    //     .delete('/api/v1/adhyayan/cancel')
-    //     .send({ bookingid: booking.id });
-
-    //   expect(res.status).toBe(400);
-    //   expect(res.body).toEqual({
-    //     error: 'Booking is already cancelled',
-    //   });
-    // });
   });
-
 
   afterAll(async () => {
     await sequelize.close();
